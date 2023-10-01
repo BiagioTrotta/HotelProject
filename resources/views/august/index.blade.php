bad9f36da314081286647633e45a9f5733c7f735
Biagio <trottabiagio94@gmail.com>
2023-10-01 13:19:39 +0200

    divisione

--- resources/views/august/index.blade.php
+++ resources/views/august/index.blade.php
@@ -41,17 +41,22 @@
         </form>
     </div>
 
-
-    <div class="table-responsive">
+    <h3>Primo Piano</h3>
+    
+    <div class="table-scroll-xy mb-5">
         <table class="table-bordered table table-striped table-hover">
             <thead>
                 <tr>
                     <th>N.Room</th>
                     @for ($day = 1; $day <= 31; $day++) <th>Day {{ $day }}</th>
                         @endfor
+                        <th>N.Room</th>
                 </tr>
             </thead>
             <tbody>
+            @php
+                $count = 0;
+                @endphp
                 @foreach ($data as $row)
                 <tr>
                     <td class="text-center">{{ $row->room->numero }}</td>
@@ -82,9 +87,75 @@
                         </td>
                         @endif
                         @endfor
+                        <td class="text-center">{{ $row->room->numero }}</td>
                 </tr>
+                @php
+                $count++;
+                if ($count == 12) break; // Termina il ciclo dopo 12 righe
+                @endphp
                 @endforeach
             </tbody>
         </table>
     </div>
+    
+
+
+
+
+<h3>Secondo Piano</h3>
+
+
+
+
+
+
+    <div class="table-scroll-xy mt-5">
+    <table class="table-bordered table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>N.Room</th>
+                    @for ($day = 1; $day <= 31; $day++) <th>Day {{ $day }}</th>
+                        @endfor
+                </tr>
+            </thead>
+            <tbody>
+            @foreach ($data as $row)
+            @if ($loop->index < 12) @continue @endif
+                <tr>
+                    <td class="text-center">{{ $row->room->numero }}</td>
+
+                    @for ($day = 1; $day <= 31; $day++)
+                    
+                     @if($row->{'day_'.$day.'_user_id'})
+                        <td class="bg-success">
+                            <form method="POST" action="{{ route('august-days.update', $row->id) }}">
+                                @csrf
+                                @method('PUT')
+                                <div style="display: flex; align-items: center; justify-content: center;">
+                                    <input type="number" name="day_{{ $day }}_user_id" value="{{ $row->{'day_'.$day} }}" placeholder="#{{ $row->{'day_'.$day.'_user_id'} }}" style="margin-right: 10px; width: 60px; padding: 5px;">
+                                    <button class="btn btn-outline-light" type="submit" style="margin-right: 10px;"><i class="fa-solid fa-lock"></i></button>
+                                </div>
+                                <span class="fw-small font-monospace text-uppercase mx-2 text-white">{{ $users[$row->{'day_'.$day.'_user_id'}-1]->name ?? '' }}</span>
+                            </form>
+                        </td>
+                        @else
+                        <td class="bg-light">
+                            <form method="POST" action="{{ route('august-days.update', $row->id) }}">
+                                @csrf
+                                @method('PUT')
+                                <div style="display: flex; align-items: center;">
+                                    <input type="number" name="day_{{ $day }}_user_id" value="{{ $row->{'day_'.$day} }}" placeholder="{{ $row->{'day_'.$day.'_user_id'} }}" style="margin-right: 10px; width: 60px; padding: 5px;">
+                                    <button class="btn btn-outline-dark" type="submit" style="margin-right: 10px;"><i class="fa-solid fa-lock-open"></i></button>
+                                </div>
+                                <span class="fw-bold text-uppercase mx-2">Not Client</span>
+                            </form>
+                        </td>
+                        @endif
+                        @endfor
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        </div>
+
 </x-main>
