9e79c9461dfffdc710c8a8127904509f9d89eec3
biagio trotta <trottabiagio94@gmail.com>
2023-10-03 20:41:25 +0200

    pulizia codice

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,9 +8,6 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $title = 'Articles';
@@ -26,18 +23,12 @@ class ArticleController extends Controller
         return redirect()->back()->with('message', 'Article successfully accepted');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         $title = 'Create Article';
         return view('articles.create', compact('title'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(ArticlesRequest $request)
     {
         $validatedData = $request->validated();
@@ -54,18 +45,12 @@ class ArticleController extends Controller
             ->with(['success' => 'Article successfully added']);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Article $article)
     {
         $title = 'Show';
         return view('articles.show', compact('title', 'article'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Article $article)
     {
         if ($article->user_id != auth()->user()->id && auth()->user()->is_admin != 1) {
@@ -75,9 +60,6 @@ class ArticleController extends Controller
         return view('articles.edit', compact('title', 'article'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(ArticlesRequest $request, Article $article)
     {
         //controllo permessi per editare articolo
@@ -94,24 +76,19 @@ class ArticleController extends Controller
         //update dei dati all'interno dell'articolo selezionato
         $article->update($request->all());
         return redirect()->back()
-        ->with(['success' => 'Article successfully modified']);
+            ->with(['success' => 'Article successfully modified']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Article $article)
     {
-        if(auth()->check() != true)
-        {
+        if (auth()->check() != true) {
             abort(403);
         }
-        if ($article->user_id != auth()->user()->id && auth()->user()->is_admin != 1)
-        {
+        if ($article->user_id != auth()->user()->id && auth()->user()->is_admin != 1) {
             abort(403);
         }
         $article->delete();
         return redirect()->back()
-        ->with(['success' => 'Article delete successfully']);
+            ->with(['success' => 'Article delete successfully']);
     }
 }
